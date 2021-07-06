c6fded773bdeb3c375c8d171c569990ab1c90526
Admin i <ihsanmpm@gmail.com>
2021-07-06 12:05:54 +0530

    Add CustomerDAO dependency Injection to CustomerController.

--- src/com/ihs2code/springdm/controller/CustomerController.java
+++ src/com/ihs2code/springdm/controller/CustomerController.java
@@ -1,15 +1,31 @@
 package com.ihs2code.springdm.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.ihs2code.springdm.dao.CustomerDAO;
+import com.ihs2code.springdm.entity.Customer;
+
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
-
+	
+	@Autowired
+	private CustomerDAO customerDAO;
+	
+	
 	@RequestMapping("/list")
 	public String listCustomers(Model theModel) {
+		
+		// get customers from the dao
+		List<Customer> theCustomers = customerDAO.getCustomers();
+		
+		// add the customers to the model
+		theModel.addAttribute("customers",  theCustomers);
 		
 		return "list-customers";
 	}
---
+++ src/com/ihs2code/springdm/dao/CustomerDAO.java
@@ -0,0 +1,10 @@
+package com.ihs2code.springdm.dao;
+
+import java.util.List;
+
+import com.ihs2code.springdm.entity.Customer;
+
+public interface CustomerDAO {
+
+	public List<Customer> getCustomers();
+}
